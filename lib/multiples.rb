e946f5dfb257b79fee9b492ffc5796479e5da33b
github username <github email address>
2020-05-06 13:46:12 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    limit -= 1
+    (1..limit).select {|x| x%3==0 or x%5==0}
+end
+
+def sum_multiples(limit)
+    a = collect_multiples(limit)
+    a.sum
+end
